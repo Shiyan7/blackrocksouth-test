2106df8726488944ec4026288dc909750f9e4364
Shiyan7 <jeka.shiyan@gmail.com>
2024-04-04 10:37:39 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { Application } from "./app";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>
+);
+
+reportWebVitals();
